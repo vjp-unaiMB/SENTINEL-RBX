24d09c74a172e1fee6be62d32f5ca7429a6a0505
Unai <umerinob01@informatica.iesvalledeljerteplasencia.es>
2025-04-28 21:48:49 +0200

    c32

--- Frontend/cambiosDOM.js
+++ Frontend/cambiosDOM.js
@@ -32,7 +32,7 @@ function actualizarListaJugadores(jugadores) {
             })
             .catch(() => {
                 // Fallback a la URL antigua si la API falla
-                img.src = `https://www.roblox.com/headshot-thumbnail/image?userId=${jugador.userId}&width=400&height=400&format=png`;
+                img.src = `https://www.roblox.com/headshot-thumbnail/image?userId=${jugador.userId}&width=420&height=420&format=png`;
             });
 
         const jugadorElement = document.createElement('div');
